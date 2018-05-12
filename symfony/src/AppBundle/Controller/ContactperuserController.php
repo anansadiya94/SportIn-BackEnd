135a7cadd65a523e89c382bddf9610cc31db95d9
1389961 <franciscojavier.martinezm@e-campus.uab.cat>
2018-05-12 21:28:29 +0200

    arreglado problema mio al subir error andres

--- symfony/src/AppBundle/Controller/ContactperuserController.php
+++ symfony/src/AppBundle/Controller/ContactperuserController.php
@@ -23,11 +23,8 @@ class ContactperuserController extends Controller
 
         $em = $this->getDoctrine()->getManager(); // ...or getEntityManager() prior to Symfony 2.1
         $connection = $em->getConnection();
-        $statement = $connection->prepare("INSERT INTO Contactperuser(userId, contact_userId) SELECT $userId, $contact_userId 
-        FROM DUAL WHERE NOT EXISTS (SELECT userId,contact_userId FROM Contactperuser c 
-        WHERE c.userId=$userId AND c.contact_userId=$contact_userId)");
-        //$statement = $connection->prepare("SELECT * FROM User INNER JOIN ContactPerUser ON ContactPerUser.contact_userId=User.userId
-        //WHERE ContactPerUser.userId=$userid");
+        $statement = $connection->prepare("SELECT * FROM User INNER JOIN ContactPerUser ON ContactPerUser.contact_userId=User.userId
+        WHERE ContactPerUser.userId=$userid");
         $statement->execute();
         return new JsonResponse($statement->fetchAll());
 
@@ -35,7 +32,7 @@ class ContactperuserController extends Controller
     //POST /contact
     // {"userId" : 1,"contactUserId" : 2}
     public function contactAction(Request $request){
-
+/*
         // obtener el servicio que me permitirá convertir a JSON
         $helpers = $this->get("app.helpers");
         // obtener los datos de la petición
@@ -53,6 +50,23 @@ class ContactperuserController extends Controller
         // Decirle que haga los cambios en BD
         $manager->flush();
         //return necesario
+*/
+
+        // obtener el servicio que me permitirá convertir a JSON
+        $helpers = $this->get("app.helpers");
+        // obtener los datos de la petición
+        $json_params = $request->get("json", null);
+
+        $userId = json_decode($json_params)->{"userId"};
+        $contact_userId = json_decode($json_params)->{"contact_userId"};
+        $em = $this->getDoctrine()->getManager(); // ...or getEntityManager() prior to Symfony 2.1
+        $connection = $em->getConnection();
+
+        $statement = $connection->prepare("INSERT INTO Contactperuser(userId, contact_userId) 
+        SELECT $userId, $contact_userId FROM DUAL WHERE NOT EXISTS 
+        (SELECT userId,contact_userId FROM Contactperuser c 
+        WHERE c.userId=$userId AND c.contact_userId=$contact_userId)");
+        $statement->execute();
         return $helpers->json(
             array(
                 "status" => "OK",
